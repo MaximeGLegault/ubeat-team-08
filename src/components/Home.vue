761bf72ece77aa7f99a8668252471359a97bf17a
fadichaouachi <34294840+fadichaouachi@users.noreply.github.com>
2018-02-11 13:02:02 -0500

    bouton style

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,42 +1,44 @@
 <template>
   <div>
     <div class="container homeContainer">
-      <h3 class="titleHeader">Welcome</h3>
-      <p class="titleText">With UBeat you can listen to your favorite tracks.Create playlist and share it with your friends  </p>
-      <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      <h3 class="titleHeader">Discover</h3>
+      <div class="playlistChoice">
+        <a class="play_circle_outline deep-purple  btn playlistBtn" data-target="#myCarousel" data-slide-to="0" ><i class="material-icons left">play_circle_outline</i>Rap</a>
+        <a class="play_circle_outline deep-purple btn playlistBtn" data-target="#myCarousel" data-slide-to="1"><i class="material-icons left">play_circle_outline</i>Rock</a>
+        <a class="play_circle_outline deep-purple btn playlistBtn" data-target="#myCarousel" data-slide-to="2"><i class="material-icons left">play_circle_outline</i>Classical</a>
+        <a class="play_circle_outline deep-purple btn playlistBtn" data-target="#myCarousel" data-slide-to="3"><i class="material-icons left">play_circle_outline</i>Pop</a>
+        <a class="play_circle_outline deep-purple btn playlistBtn" data-target="#myCarousel" data-slide-to="4"><i class="material-icons left">play_circle_outline</i>Electro</a>
+      </div>
+
+     <div id="myCarousel" class="carousel slide" data-ride="carousel">
         <!-- Indicators -->
-        <ol class="carousel-indicators">
-          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-          <li data-target="#myCarousel" data-slide-to="1"></li>
-          <li data-target="#myCarousel" data-slide-to="2"></li>
-          <li data-target="#myCarousel" data-slide-to="3"></li>
-          <li data-target="#myCarousel" data-slide-to="4"></li>
+       <!-- <ol class="carousel-indicators">
+          <li ></li>
+          <li ></li>
+          <li ></li>
+          <li ></li>
+          <li ></li>
         </ol>
+        -->
 
         <!-- Wrapper for slides -->
         <div class="carousel-inner">
           <div class="item active">
-            <a class=" btn accent-3 boutonCarousel">Rap<i class="material-icons">play_arrows</i></a>
             <img src="http://dailychiefers.com/wp-content/media/2015/07/drake-to-the-stage-HD-Wallpaper.jpg" alt="Rap" style="width:100%;">
-
           </div>
 
           <div class="item">
-            <a class=" btn  accent-3 boutonCarousel">Rock<i class="material-icons">play_arrows</i></a>
             <img src="http://www.musiclipse.com/wp-content/uploads/2014/06/amazing_ac_dc_brian_johnson_and_angus_young_wallpaper.jpg" alt="Rock" style="width:100%;">
           </div>
 
           <div class="item">
-            <a class=" btn  accent-3 boutonCarousel">Classical<i class="material-icons">play_arrows</i></a>
             <img src="https://wwwcmuseorg-lvzm5mr0z.stackpathdns.com/wp-content/uploads/2017/04/introduce-classical-music-to-children.jpg" alt="Classical" style="width:100%;">
           </div>
 
           <div class="item">
-            <a class=" btn accent-3 boutonCarousel">Pop<i class="material-icons">play_arrows</i></a>
             <img src="https://i.ytimg.com/vi/DsNK8iKEh00/maxresdefault.jpg" alt="Pop" style="width:100%;">
           </div>
           <div class="item">
-            <a class=" btn accent-3 boutonCarousel">Electro<i class="material-icons">play_arrows</i></a>
             <img src="https://cdn01.masterstudies.com/element_db/52/5261_Electronics_Music_Production_pic_1.jpg" alt="electro" style="width:100%;">
           </div>
         </div>
@@ -53,11 +55,11 @@
       </div>
 
     </div>
+
   </div>
 </template>
 
 <style>
-  /***/
   /*{*/
   /*box-sizing: border-box;*/
   /*}*/
@@ -74,7 +76,6 @@
     transform: translate(-50%, -50%);
     -ms-transform: translate(-50%, -50%);
     background-color: grey;
-    font-size: 16px;
     border: none;
     cursor: pointer;
     border-radius: 100px;
@@ -83,30 +84,40 @@
  .boutonCarousel:hover{
    background-color:#651fff  ;
  }
-
+  .titleText
+  {
+    font-size: 20px;
+  }
   .titleHeader, .titleText{
     text-align: center;
+  }
+  .playlistChoice
+  {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    margin: 5px;
+    width: 100%;
+    justify-content: space-between;
+  }
+  .playlistBtn
+  {
+
   }
 
   @media only screen and (max-width: 1100px)
   {
-    .homeContainer
-    {
-
-    }
+ 
   }
   @media only screen and (max-width: 798px)
   {
-    .homeContainer
-    {
 
-    }
   }
   @media only screen and (min-device-width : 320px) and (max-device-width : 480px)
   {
-    .homeContainer
+    .titleText
     {
-      margin: 400px 100px;
+      font-size: 24px;
     }
 
   }
