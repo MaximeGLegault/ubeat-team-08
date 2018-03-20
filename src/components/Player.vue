495584a882c1f9dff83f12251ef62bbf15645664
Maxime Gagnon-Legault <maxime.glegault@gmail.com>
2018-03-20 16:50:36 -0400

    removed old player, added buttons

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -1,43 +1,29 @@
 <template>
     <div id="player">
-       <Aplayer style="width: 90%" v-if="shown"
-                :music.sync="currentTrack"
-                :list="currentTracks"
-                :autoplay="true"
-                :theme="'#e1e1e1'"
-                :showLrc="false"
-                :listMaxHeight="'0'"/>
+      <div id="thumbnail">
+        <img src="http://is3.mzstatic.com/image/thumb/Music20/v4/33/47/ea/3347ea2b-5628-9283-da07-bcfb58e597d0/source/100x100bb.jpg"
+             :click="$emit('jhg')"/>
+      </div>
+      <div id="buttons">
+        <div id="left_buttons">
+          <a class=" btn deep-purple accent-3 btn-floating"><i class="material-icons md-48">skip_previous</i></a>
+        </div>
+        <div id="middle_button">
+          <a class=" btn deep-purple accent-3 btn-floating"><i class="material-icons md-48">play_arrow</i></a>
+        </div>
+        <div id="right_buttons">
+          <a class=" btn deep-purple accent-3 btn-floating"><i class="material-icons md-48">skip_next</i></a>
+        </div>
+      </div>
     </div>
 </template>
 
 <script>
-  import util from '@/lib/util';
-  import Aplayer from 'vue-aplayer';
 
   export default {
     components: {
-      Aplayer,
     },
     computed: {
-      shown() {
-        return (this.$store.state.currentPlaylist &&
-          this.$store.state.currentPlaylist.tracks &&
-          this.$store.state.currentPlaylist.tracks.length > 0);
-      },
-      currentTrack() {
-        if (this.currentTracks.length > 0) {
-          return this.currentTracks[0];
-        }
-        return null;
-      },
-      currentTracks() {
-        if (this.$store.state.currentPlaylist &&
-          this.$store.state.currentPlaylist.tracks &&
-          this.$store.state.currentPlaylist.tracks.length > 0) {
-          return util.getMusicInfo(this.$store.state.currentPlaylist.tracks);
-        }
-        return [];
-      }
     }
   };
 </script>
@@ -47,8 +33,38 @@
     background-color: #1e1e1e;
     position: fixed;
     width: 100%;
-    height: 10vh;
+    height: 100px;
     bottom: 0;
     left: 0;
+    display: inline-flex;
+  }
+
+  #thumbnail {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    height: 100px;
+  }
+
+  #buttons {
+    display: inline-flex;
+    position: relative;
+    flex-flow: row nowrap;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+  }
+
+  #left_buttons {
+
+  }
+
+  #middle_button {
+
+  }
+
+  right_buttons {
+
   }
 </style>
