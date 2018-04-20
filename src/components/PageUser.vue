397cf4219d6927c5b6ddbf3fbee2fc4083a27392
fadichaouachi <34294840+fadichaouachi@users.noreply.github.com>
2018-04-20 10:13:27 -0400

    userProfil
    not finished!!!

--- src/components/PageUser.vue
+++ src/components/PageUser.vue
@@ -1,25 +1,54 @@
 <template>
-  <div>
+  <div id="userOverview">
+    <avatar username="f c"></avatar>
     <h1>{{this.$store.state.userName}} </h1>
-    <h1>{{this.$store.state.user_email}} </h1>
-    <h1>{{this.$store.state.userPlaylists}} </h1>
+    <h1>{{this.$store.state.email}} </h1>
+    <h1>{{this.$store.state.userPlaylists}}</h1>
+     <a class="waves-effect waves-light btn">Follow</a>
 
+
+    <!--friends-->
+    <div class="result" v-for="result of results" :key="result.index">
+      <div class="resultType">
+        <div class="resultJacket">
+          <!--<router-link :to="{ name : 'Artist', params: { artistId: result.artistId }}">-->
+          <img src="../assets/empty-user-photo.png"/>
+          <!--</router-link>-->
+        </div>
+        <div class="resultName">
+          <div class="resultTitle">{{result.name}}</div>
+          <div class="resultInfo">Users</div>
+        </div>
+      </div>
+    </div>
+    <!--end of friends-->
   </div>
+
 </template>
 
 <script>
-  import Cookies from 'js-cookie';
+import Avatar from 'vue-avatar';
+import util from '@/lib/util';
 
-  export default {
-    data: () => ({
-    }),
-    beforeCreate() {
-      if (Cookies.get('token') === '') {
-        this.$router.push('/login');
-      }
-    }
-  };
+export default {
+  name: 'ResultByGlobal',
+  props: {
+    results: {
+      type: Array,
+      required: false
+    },
+  },
+  methods: {
+    artwork(artworkUrl, dimension) {
+      return util.getUrlOfBiggerAlbumArtwork(artworkUrl, dimension);
+    },
+  },
+  components: {
+    Avatar
+  },
+};
 </script>
 
 <style>
+
 </style>
